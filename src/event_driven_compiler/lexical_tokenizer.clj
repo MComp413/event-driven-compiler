cf9b8a261362b6c929e0868b2a4e5d7a9bd1fbfe
matheus.andrade2 <matheus.andrade@inspira.com.br>
2021-09-29 17:43:39 -0300

    Adiciona construtores de autômato de pilha; Adiciona autômato de pilha para tokenização léxica

--- src/event_driven_compiler/lexical_tokenizer.clj
+++ src/event_driven_compiler/lexical_tokenizer.clj
@@ -1,6 +1,7 @@
 (ns event-driven-compiler.lexical-tokenizer
   (:require [event-driven-compiler.event-engine :as ngn]
             [event-driven-compiler.event-queue :as q]
+            [event-driven-compiler.stack-automata :as stk-atm]
             [clojure.string :as string]))
 
 ; Estado do motor léxico
@@ -12,7 +13,6 @@
 (defn is-whitespace-char?
   [character]
   (not-empty (re-seq blank-characters-regex (str character))))
-
 
 (def word-character-regex #"[A-Za-z]")
 (defn is-word-char?
@@ -42,6 +42,137 @@
 ;;                 ()))))))
 
 
+; Autômato de tokens léxicos
+(def token-builder-transitions
+  {:empty (fn [stack event]
+            (let [next-char (-> event :data)
+                  char-type (get-char-category next-char)]
+              (cond char-type
+                    :word-char
+                    (stk-atm/new-automaton-state :word
+                                                 (str stack next-char)
+                                                 token-builder-transitions
+                                                 [])
+                    :digit-char
+                    (stk-atm/new-automaton-state :number
+                                                 (str stack next-char)
+                                                 token-builder-transitions
+                                                 [])
+                    :whitespace
+                    (stk-atm/new-automaton-state :empty
+                                                 stack
+                                                 token-builder-transitions
+                                                 [])
+                    :special-char
+                    (stk-atm/new-automaton-state :special
+                                                 (str  stack next-char)
+                                                 token-builder-transitions
+                                                 [])
+
+                    (stk-atm/new-automaton-state :error
+                                                 stack
+                                                 token-builder-transitions
+                                                 []))))
+   :word (fn [stack event]
+           (let [next-char (-> event :data)
+                 char-type (get-char-category next-char)]
+             (case char-type
+               (:word-char :digit-char)
+               (stk-atm/new-automaton-state :word
+                                            (str stack next-char)
+                                            token-builder-transitions
+                                            [])
+               :whitespace
+               (stk-atm/new-automaton-state :empty
+                                            ""
+                                            token-builder-transitions
+                                            [(ngn/new-event :word-token
+                                                            (+ 1 (-> event :timestamp))
+                                                            stack)])
+               :special-char
+               (stk-atm/new-automaton-state :special
+                                            (str next-char)
+                                            token-builder-transitions
+                                            [(ngn/new-event :word-token
+                                                            (+ 1 (-> event :timestamp))
+                                                            stack)])
+               (stk-atm/new-automaton-state :error
+                                            stack
+                                            token-builder-transitions
+                                            []))))
+   :number (fn [stack event]
+             (let [next-char (-> event :data)
+                   char-type (get-char-category next-char)]
+               (case char-type
+                 :word-char
+                 (stk-atm/new-automaton-state :error
+                                              stack
+                                              token-builder-transitions
+                                              [])
+                 :digit-char
+                 (stk-atm/new-automaton-state :number
+                                              (str stack next-char)
+                                              token-builder-transitions
+                                              [])
+                 :special-char
+                 (stk-atm/new-automaton-state :special
+                                              (str next-char)
+                                              token-builder-transitions
+                                              [(ngn/new-event :number-token
+                                                              (+ 1 (-> event :timestamp))
+                                                              stack)])
+                 :whitespace
+                 (stk-atm/new-automaton-state :empty
+                                              (str next-char)
+                                              ""
+                                              [(ngn/new-event :number-token
+                                                              (+ 1 (-> event :timestamp))
+                                                              stack)])
+                 (stk-atm/new-automaton-state :error
+                                              stack
+                                              token-builder-transitions
+                                              []))))
+   :special (fn [stack event]
+              (let [next-char (-> event :data)
+                    char-type (get-char-category next-char)]
+                (case char-type
+                  :word-char
+                  (stk-atm/new-automaton-state :word
+                                               (str next-char)
+                                               token-builder-transitions
+                                               [(ngn/new-event :special-token
+                                                               (+ 1 (-> event :timestamp))
+                                                               stack)])
+                  :digit-char
+                  (stk-atm/new-automaton-state :number
+                                               (str next-char)
+                                               token-builder-transitions
+                                               [(ngn/new-event :special-token
+                                                               (+ 1 (-> event :timestamp))
+                                                               stack)])
+                  :special-char
+                  (stk-atm/new-automaton-state :special
+                                               (str stack next-char)
+                                               token-builder-transitions
+                                               [])
+                  :whitespace
+                  (stk-atm/new-automaton-state :empty
+                                               ""
+                                               token-builder-transitions
+                                               [(ngn/new-event :special-token
+                                                               (+ 1 (-> event :timestamp))
+                                                               stack)]))))})
+
+(defn get-next-token-builder-state
+  [state-type stack event]
+  ((token-builder-transitions state-type) stack event))
+
+(def token-builder-automata
+  (stk-atm/create-stack-automaton
+   :empty
+   ""
+   token-builder-transitions))
+
 ; Gerenciadores de evento léxico
 (def lexical-event-handlers
   {:start (fn [event]
@@ -60,35 +191,23 @@
            (apply
             (lexical-queue :push)
             (for [character (seq (-> event :data))]
-              (ngn/new-event (get-char-category character)
+              (ngn/new-event :character
                              (+ (-> event :timestamp) 1)
                              character))))
-  ;;  :character (fn [event]
-  ;;               ((lexical-queue :push)
-  ;;                (if (not= (-> event :data) :whitespace)
-  ;;                  ((lexical-queue :push)
-  ;;                   (ngn/new-event (get-char-category (-> event :data))
-  ;;                                  (+ (-> event :timestamp) 1)
-  ;;                                  (-> event :data)))
-  ;;                  (cond (not-empty @token-builder)
-  ;;                        (do
-  ;;                        ;colocar evento de token na fila e esvaziar palavra
-  ;;                          ((lexical-queue :push)
-  ;;                           (ngn/new-event :token
-  ;;                                          (+ (-> event :timestamp) 1)
-  ;;                                          (str @token-builder)))
-  ;;                          (dosync
-  ;;                           (alter token-builder
-  ;;                                  (fn [] ""))))))))
-   :word-char (fn [event]
-                (dosync
-                 (alter token-builder
-                        (fn [state]
-                          (str state (-> event :data))))))
-   :digit-char (fn [event])
-   :special-char (fn [event])
-   :whitespace (fn [event])
-   :token (fn [event])})
+   :character (fn [event]
+                (let [state @token-builder-automata
+                      next-state (get-next-token-builder-state (state :state-type) (state :stack) event)
+                      events-to-push (next-state :outgoing-events)]
+                  (dosync
+                   (alter
+                    token-builder-automata
+                    (constantly next-state)))
+                  (apply
+                   (lexical-queue :push)
+                   events-to-push)))
+   :word-token (fn [event])
+   :number-token (fn [event])
+   :special-token (fn [event])})
 
 (defn lexical-event-handler-selector
   [event]
